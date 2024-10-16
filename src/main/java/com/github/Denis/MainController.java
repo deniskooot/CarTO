cc1c29d2c185e3a51a649c7f8ff7a623812bd61c
deniskooot <deniskooot@gmail.com>
2024-10-16 17:54:58 +0300

    added all files

---
+++ src/main/java/com/github/Denis/MainController.java
@@ -0,0 +1,19 @@
+package com.github.Denis;
+
+import org.apache.catalina.webresources.FileResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/") //этот контроллер отвечает за эту директорию
+public class MainController {
+
+
+
+    @GetMapping // обработчик get запроса
+    public String showStatus() {
+        return "Hello, world!";
+    }
+
+}
