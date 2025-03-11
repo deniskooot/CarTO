bb0982806cce386521c56152482579da0a35262b
deniskooot <deniskooot@gmail.com>
2025-03-11 19:43:27 +0300

    Add CorsConfig Add CorsConfig class to allow frontend use  port. Delete annotation @CrossOrigin for CarsController.

---
+++ src/main/java/com/github/Denis/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.github.Denis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Разрешаем все запросы к API
+                        .allowedOrigins("http://localhost:3000") // Разрешаем фронту
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Разрешенные методы
+                        .allowedHeaders("*"); // Разрешаем любые заголовки
+            }
+        };
+    }
+}
