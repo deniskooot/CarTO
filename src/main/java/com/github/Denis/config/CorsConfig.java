70b4375091fbdbc33431f2e7f0dc111880a31c54
Vadim Barkov <vbrkov@gmail.com>
2026-02-25 22:06:02 +0800

    Final fixes

--- src/main/java/com/github/Denis/config/CorsConfig.java
+++ src/main/java/com/github/Denis/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.github.Denis.config;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -14,7 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/api/**") // Разрешаем все запросы к API
                         .allowedOrigins("http://localhost:3000") // Разрешаем фронту
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Разрешенные методы
@@ -24,6 +25,10 @@ public class CorsConfig implements WebMvcConfigurer {
     }
 
 
+    /**
+     * Перенаправление на Swagger UI при доступе к корню приложения.
+     * Просто удобно.
+     */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("redirect:/swagger-ui.html");
