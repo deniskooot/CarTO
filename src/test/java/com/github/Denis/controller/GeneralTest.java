5bec8084523971a5d77c91ff8d43d52a42ef7bf8
Vadim Barkov <vbrkov@gmail.com>
2026-02-26 11:33:41 +0800

    Added basic test that context is loading

---
+++ src/test/java/com/github/Denis/controller/GeneralTest.java
@@ -0,0 +1,20 @@
+package com.github.Denis.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+/**
+ * Проверяет что вообще приложение загружается нормально.
+ * Используется временный контейнер postgres для тестов, прогоняются миграции.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("tc")
+@Testcontainers
+public class GeneralTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
